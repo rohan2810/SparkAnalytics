878cc8c63e4b3bfe404246e7a1a689e6b7458a1b
rohan <rohansurana2810@gmail.com>
2020-08-19 10:57:33 -0700

    activeMq

---
+++ src/main/java/ActiveMq/ReceiveMessage.java
@@ -0,0 +1,45 @@
+package ActiveMq;
+
+import org.apache.activemq.ActiveMQConnection;
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+
+public class ReceiveMessage {
+    // URL of the JMS server
+    private static String url = ActiveMQConnection.DEFAULT_BROKER_URL;
+    // default broker URL is : tcp://localhost:61616"
+
+    // Name of the queue we will receive messages from
+    private static String subject = "JCG_QUEUE";
+
+    public static void main(String[] args) throws JMSException {
+        // Getting JMS connection from the server
+        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(url);
+        Connection connection = connectionFactory.createConnection();
+        connection.start();
+
+        // Creating session for seding messages
+        Session session = connection.createSession(false,
+                Session.AUTO_ACKNOWLEDGE);
+
+        // Getting the queue 'JCG_QUEUE'
+        Destination destination = session.createQueue(subject);
+
+        // MessageConsumer is used for receiving (consuming) messages
+        MessageConsumer consumer = session.createConsumer(destination);
+
+        // Here we receive the message.
+        Message message = consumer.receive(1000);
+
+        // We will be using TestMessage in our example. MessageProducer sent us a TextMessage
+        // so we must cast to it to get access to its .getText() method.
+        if (message instanceof TextMessage) {
+            TextMessage textMessage = (TextMessage) message;
+            System.out.println("Received message '" + textMessage.getText() + "'");
+        }
+        consumer.close();
+        session.close();
+        connection.close();
+    }
+}
---
+++ src/main/java/ActiveMq/SendMessage.java
@@ -0,0 +1,38 @@
+package ActiveMq;
+
+import org.apache.activemq.ActiveMQConnection;
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+
+public class SendMessage {
+    private static final String url = ActiveMQConnection.DEFAULT_BROKER_URL;
+
+    public static void main(String[] args) throws JMSException {
+        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(url);
+        Connection connection = connectionFactory.createConnection();
+        connection.start();
+
+        //Creating a non transactional session to send/receive JMS message.
+        Session session = connection.createSession(false,
+                Session.AUTO_ACKNOWLEDGE);
+
+        //Destination represents here our queue 'JCG_QUEUE' on the JMS server.
+        //The queue will be created automatically on the server.
+        String subject = "JCG_QUEUE";
+        Destination destination = session.createQueue(subject);
+
+        // MessageProducer is used for sending messages to the queue.
+        MessageProducer producer = session.createProducer(destination);
+
+        // We will send a small text message saying 'Hello World!!!'
+        TextMessage message = session
+                .createTextMessage("Hello !!! Welcome to the world of ActiveMQ.");
+
+        // Here we are sending our message!
+        producer.send(message);
+
+        System.out.println("JCG printing@@ '" + message.getText() + "'");
+        connection.close();
+    }
+}
